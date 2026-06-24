29f895ce79778250ea2e2f59fb30c5ca98621095
gttmas <gabtomas26@hotmail.com>
2026-06-24 00:14:15 -0300

    codigo_esp

---
+++ code_esp/comunica_esp/comunica_esp.ino
@@ -0,0 +1,103 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+#define ID_MINHA_EQUIPE 2026
+
+// 1. Endereço MAC de Broadcast
+uint8_t mac_broadcast[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+typedef struct mensagem_mlt3 {
+  uint16_t id_grupo;
+  int8_t niveis[150];
+  int tamanho;
+} mensagem_mlt3;
+
+mensagem_mlt3 pacote_envio;
+mensagem_mlt3 pacote_recebido;
+
+// ================================================================
+// FUNÇÃO RX: Recebe do ar (ESP-NOW) e manda pro Python (Cabo USB)
+// ================================================================
+void aoReceberDados(const uint8_t * mac, const uint8_t *dadosRecebidos, int len) {
+  // Copia os dados que chegaram pelo ar para a nossa variável
+  memcpy(&pacote_recebido, dadosRecebidos, sizeof(pacote_recebido));
+
+  if (pacote_recebido.id_grupo != ID_MINHA_EQUIPE) {
+    return; // Sai da função imediatamente e ignora o pacote intruso
+  }
+  
+  // Imprime no cabo USB com o prefixo "RX:" para o Python saber que é uma mensagem
+  Serial.print("RX:"); 
+  
+  for (int i = 0; i < pacote_recebido.tamanho; i++) {
+    Serial.print(pacote_recebido.niveis[i]);
+    if (i < pacote_recebido.tamanho - 1) {
+      Serial.print(","); // Adiciona vírgulas, menos no último número
+    }
+  }
+  Serial.println(); // Envia quebra de linha avisando o Python que terminou
+}
+
+// ================================================================
+// SETUP: Configurações de Rede e Rádio
+// ================================================================
+void setup() {
+  Serial.begin(115200); // Mesma velocidade que você colocará no Python (pyserial)
+  
+  // Liga o Wi-Fi no modo Station (Padrão)
+  WiFi.mode(WIFI_STA);
+  
+  // Inicializa o protocolo ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Erro ao inicializar ESP-NOW");
+    return;
+  }
+  
+  // Registra a função de escuta (quando chegar Wi-Fi, roda a função acima)
+  esp_now_register_recv_cb(esp_now_recv_cb_t(aoReceberDados));
+
+  // Configura a quem vamos enviar as mensagens (Neste caso: Broadcast / Todos)
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, mac_broadcast, 6);
+  peerInfo.channel = 0;  // Canal 0 usa o canal atual do Wi-Fi
+  peerInfo.encrypt = false;
+  
+  // Adiciona o endereço de Broadcast à lista de permissões
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Erro ao registrar o Broadcast");
+    return;
+  }
+}
+
+// ================================================================
+// LOOP (TX): Ouve o Python (Cabo USB) e manda pro ar (ESP-NOW)
+// ================================================================
+void loop() {
+  if (Serial.available()) {
+    // Lê a mensagem do Python até ele dar um (\n)
+    String recebido_do_pc = Serial.readStringUntil('\n'); 
+    recebido_do_pc.trim();
+    
+    if (recebido_do_pc.length() > 0) {
+
+      pacote_envio.id_grupo = ID_MINHA_EQUIPE;
+      char buffer[500]; 
+      recebido_do_pc.toCharArray(buffer, sizeof(buffer));
+      
+      // Quebra a string nas vírgulas
+      char* token = strtok(buffer, ",");
+      int indice = 0;
+      
+      // Converte cada fatia de texto em número e salva no pacote
+      while (token != NULL && indice < 150) {
+        pacote_envio.niveis[indice] = atoi(token); 
+        indice++;
+        token = strtok(NULL, ","); 
+      }
+      
+      pacote_envio.tamanho = indice;
+      
+      esp_now_send(mac_broadcast, (uint8_t *) &pacote_envio, sizeof(pacote_envio));
+    }
+  }
+}
