c4756f39732f309e1ebc68d193177429dd2fba51
gttmas <gabtomas26@hotmail.com>
2026-07-01 14:42:43 -0300

    arruma slave

--- comunica_esp/comunica_esp.ino
+++ comunica_esp/comunica_esp.ino
@@ -17,7 +17,7 @@ typedef struct mensagem_mlt3 {
 mensagem_mlt3 pacote_envio;
 mensagem_mlt3 pacote_recebido;
 
-// função RX: Recebe e manda pro Python 
+// LOOP: Ouve o ar (ESP-NOW) e manda pro Python (Cabo USB)
 
 void aoReceberDados(const uint8_t * mac, const uint8_t *dadosRecebidos, int len) {
   memcpy(&pacote_recebido, dadosRecebidos, sizeof(pacote_recebido));
@@ -26,7 +26,6 @@ void aoReceberDados(const uint8_t * mac, const uint8_t *dadosRecebidos, int len)
     return; 
   }
   
-  // CORRIGIDO: Envia no formato exato que o Python espera -> RX:esp1:0,1,-1...
   Serial.print("RX:"); 
   Serial.print(pacote_recebido.esp_origem);
   Serial.print(":");
@@ -37,19 +36,15 @@ void aoReceberDados(const uint8_t * mac, const uint8_t *dadosRecebidos, int len)
       Serial.print(","); 
     }
   }
-  Serial.println(); // Avisa o Python que terminou a linha
+  Serial.println(); 
 }
 
 // SETUP: Configurações de Rede e Rádio
 void setup() {
-  Serial.begin(115200); // Mesma velocidade configurada no Python
+  Serial.begin(115200); 
   
-  // Liga o Wi-Fi no modo Station (Padrão)
   WiFi.mode(WIFI_STA);
   
-  // 
-  // AJUSTE DE FREQUÊNCIA: Força o ESP32 a ficar no Canal 1
-  // Garante que o Host A e o Host B sempre se encontrem no rádio
   esp_wifi_set_promiscuous(true);
   esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
   esp_wifi_set_promiscuous(false);
@@ -60,23 +55,20 @@ void setup() {
     return;
   }
   
-  // Registra a função de escuta (quando chegar dados pelo rádio, roda a função)
   esp_now_register_recv_cb(esp_now_recv_cb_t(aoReceberDados));
 
-  // Configura a quem vamos enviar as mensagens (Neste caso: Broadcast / Todos)
   esp_now_peer_info_t peerInfo = {};
   memcpy(peerInfo.peer_addr, mac_broadcast, 6);
-  peerInfo.channel = 1;  // <-- AJUSTADO: Fixo no Canal 1 para casar com o rádio
+  peerInfo.channel = 1;  
   peerInfo.encrypt = false;
   
-  // Adiciona o endereço de Broadcast à lista de permissões
   if (esp_now_add_peer(&peerInfo) != ESP_OK){
     Serial.println("Erro ao registrar o Broadcast");
     return;
   }
 }
 
-// LOOP (TX): Ouve o Python (Cabo USB) e manda pro ar (ESP-NOW)
+// LOOP: Ouve o Python (Cabo USB) e manda pro ar (ESP-NOW)
 void loop() {
   if (Serial.available()) {
     // Lê a mensagem do Python até ele dar um (\n)
@@ -86,7 +78,6 @@ void loop() {
     if (recebido_do_pc.length() > 0) {
 
       pacote_envio.id_grupo = ID_MINHA_EQUIPE;
-
       
       int pos_dois_pontos = recebido_do_pc.indexOf(':');
       String id_remetente = "Desconhecido";
@@ -97,18 +88,15 @@ void loop() {
         dados_niveis = recebido_do_pc.substring(pos_dois_pontos + 1);
       }
 
-      // Copia o nome do remetente para a struct de envio
       strncpy(pacote_envio.esp_origem, id_remetente.c_str(), sizeof(pacote_envio.esp_origem));
 
-
       char buffer[1000]; 
-      recebido_do_pc.toCharArray(buffer, sizeof(buffer));
+      dados_niveis.toCharArray(buffer, sizeof(buffer)); 
+
       
-      // Quebra a string nas vírgulas
       char* token = strtok(buffer, ",");
       int indice = 0;
       
-      // Converte cada fatia de texto em número e salva no pacote
       while (token != NULL && indice < 200) {
         pacote_envio.niveis[indice] = atoi(token); 
         indice++;
